0662ec4c7ac37a82036991826820083795fb26f1
Aldo <aldo.medina.z@gmail.com>
2023-02-15 19:00:13 -0300

    project cards uploaded

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,25 +24,53 @@ export default function Home() {
       </Head>
       <main className={s.main}>
         <MovingSquares />
+
         <div className={s.container}>
           <h2 className={classNames(ibm.className, s.h2)}>
             FINE is a web3 studio for generative and interoperable assets
           </h2>
           <h1 className={classNames(syne.className, s.h1)}>FINE</h1>
+          <div className={classNames(ibm.className, s.projects)}>
+            <div className={s.projectsWrapper}>
+              <div className={classNames(s.project, s.wargames)}>
+                <a href="https://www.wargames.app" className={s.imageWrapper}>
+                  <Image src={"/wargames-card.png"} fill alt="wargames image" />
+                </a>
+                <div className={s.text}>
+                  <p>
+                    <strong>SOLIDS Builder</strong>: 3D Generative Design
+                    Experience
+                  </p>
+                </div>
+              </div>
+              <div className={classNames(s.project, s.solid)}>
+                <a href="https://www.solids.live" className={s.imageWrapper}>
+                  <Image src={"/solids-card.png"} fill alt="solids image" />
+                </a>
+                <div className={s.text}>
+                  <p>
+                    <strong>WarGames</strong>: instantly generate your unique 2D
+                    art without code.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className={s.bottom}>
+            <a
+              className={s.image}
+              href="http://a16zcrypto.com/css23"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image src="/a16z.png" width={75} height={62} alt="a16zlogo" />
+            </a>
+            <div className={s.separator} />
             <a
               className={classNames(ibm.className, s.link)}
               href="https://twitter.com/fine_3d"
             >
               TWITTER
-            </a>
-            <div className={s.separator} />
-            <a
-              href="http://a16zcrypto.com/css23"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Image src="/a16z.png" width={100} height={83} alt="a16zlogo" />
             </a>
             <div className={s.separator} />
             <a
